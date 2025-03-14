c86a76f9d9c712d0f0dd071a586f71944558ed1a
lawlie8 <l.lawlie8@gmail.com>
2025-03-14 17:49:55 +0530

    Added MariaDb Support

---
+++ src/test/java/com/persistent/matrikas/MatrikasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.persistent.matrikas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatrikasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
