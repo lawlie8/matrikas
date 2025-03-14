409ceb3c3670334311c90619f0c14dbc1c37666a
lawlie8 <l.lawlie8@gmail.com>
2025-03-14 17:07:26 +0530

    Fixed Issue with Selected Page After Refresh

--- matrikas-ui/src/pages/global/header/Header.jsx
+++ matrikas-ui/src/pages/global/header/Header.jsx
@@ -1,11 +1,13 @@
 import './Header.css';
 import { useNavigate } from "react-router";
-import { PieChartOutlined,FireOutlined,PlusCircleOutlined, HomeOutlined } from '@ant-design/icons';
+import { PieChartOutlined,FireOutlined,PlusCircleOutlined, HomeOutlined, HomeFilled, PieChartFilled, FireFilled, PlusCircleFilled } from '@ant-design/icons';
 import { Tooltip } from 'antd'
+import { useState } from 'react';
 export default function Header(params = { params }) {
 
     const navigate = useNavigate();
-
+    const [currentSelectedPage,setCurrentSelectedPage] = useState("/"+window.location.href.split("/").pop());
+    
     return <div className="global-header">
         <ul style={{ listStyle: 'none', padding: '0px', marginTop: '20px' }}>
             <li>
@@ -17,24 +19,56 @@ export default function Header(params = { params }) {
 
         </ul>
         <ul style={{ listStyle: 'none', padding: '0px', marginTop: '50px' }}>
-        <li key={"/"} style={{cursor:'pointer'}} onClick={()=>{navigate("/")}}>
-                <Tooltip  placement="right" title="Dashboard">
-                    <HomeOutlined style={{ fontSize: '25px' }}  />
+        <li key={"/"} style={{cursor:'pointer'}} onClick={()=>{
+            setCurrentSelectedPage("/");
+            navigate("/")}}>
+                <Tooltip  placement="right" title="Home">
+                    {
+                        currentSelectedPage !== '/' ?
+                        <HomeOutlined style={{ fontSize: '25px' }} className='header-icon' />
+                         :
+                        <HomeFilled style={{ fontSize: '25px',color:'#fc6008' }} className='header-icon' /> 
+                    }
+                    
                 </Tooltip>
             </li>
-            <li key={"/dashboard"} style={{marginTop:'25px',cursor:'pointer'}} onClick={()=>{navigate("/dashboard")}}>
+            <li key={"/dashboard"} style={{marginTop:'25px',cursor:'pointer'}} onClick={()=>{
+                setCurrentSelectedPage("/dashboard");
+                navigate("/dashboard")}}>
                 <Tooltip  placement="right" title="Dashboard">
-                    <PieChartOutlined style={{ fontSize: '25px' }}  />
+                {
+                        currentSelectedPage !== '/dashboard' ?
+                        <PieChartOutlined style={{ fontSize: '25px' }} className='header-icon' />
+                         :
+                        <PieChartFilled style={{ fontSize: '25px',color:'#fc6008' }} className='header-icon' /> 
+                    }
                 </Tooltip>
             </li>
-            <li style={{marginTop:'25px',cursor:'pointer'}} onClick={()=>{navigate("/scans")}}>
+            <li style={{marginTop:'25px',cursor:'pointer'}} onClick={()=>{
+                setCurrentSelectedPage("/scans");
+                navigate("/scans")
+                }}>
                 <Tooltip  placement="right" title="Show Scans">
-                    <FireOutlined  style={{ fontSize: '25px' }} />
+                    
+                {
+                        currentSelectedPage !== '/scans' ?
+                        <FireOutlined style={{ fontSize: '25px' }} className='header-icon' />
+                         :
+                        <FireFilled style={{ fontSize: '25px',color:'#fc6008' }} className='header-icon' /> 
+                    }
                 </Tooltip>
             </li>
-            <li style={{marginTop:'25px',cursor:'pointer'}} onClick={()=>{navigate("/resources")}}>
+            <li style={{marginTop:'25px',cursor:'pointer'}} onClick={()=>{
+                setCurrentSelectedPage("/resources");
+                navigate("/resources")
+                }}>
                 <Tooltip  placement="right" title="Add Resources">
-                    <PlusCircleOutlined  style={{ fontSize: '25px' }} />
+                {
+                        currentSelectedPage !== '/resources' ?
+                        <PlusCircleOutlined style={{ fontSize: '25px' }} className='header-icon' />
+                         :
+                        <PlusCircleFilled style={{ fontSize: '25px',color:'#fc6008'}} className='header-icon' /> 
+                    }
                 </Tooltip>
             </li>
         </ul>
